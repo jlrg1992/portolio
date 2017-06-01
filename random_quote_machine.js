3801e6223f362d609f621e0ba6bc314d4d9c8a82
Jorge Luis <jlrg1992@gmail.com>
2017-06-01 16:10:05 -0500

    Update random_quote_machine.js

--- random_quote_machine.js
+++ random_quote_machine.js
@@ -1,17 +1,22 @@
-var x = 0;
+var x = 1.5;
 var quoteData = null;
 var lengthie = 0;
 function myQuote(){
   
-  if(x===0){
+  if(x===1.5){
 $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(a) {
   
   quoteData = a;
 lengthie = quoteData.length;
-
+  nextthing();
 });
 }
- x = Math.floor(Math.random() * (lengthie));
+else{
+  nextthing();
+}
+  }
+  function nextthing(){
+    x = Math.floor(Math.random() * (lengthie));
   
   var yaQue = quoteData[x]["content"];
   yaQue = yaQue.split("");
@@ -23,7 +28,6 @@ lengthie = quoteData.length;
 
    $("#citado").html(quoteData[x]["content"]);
   $("#autor").html(quoteData[x]["title"]);
-  $("#tuitera").attr("href",'https://twitter.com/intent/tweet?text='+yaQue+'See more at https://jorgerangel.ga');
-
+  $("#tuitera").attr("href",'https://twitter.com/intent/tweet?text='+yaQue+'See more at https://jorgerangel.ga/random_quote_machine.html');
   }
   myQuote();
