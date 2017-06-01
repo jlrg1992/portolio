4233aa690118131f71377c8b0ef52cec6dd108e2
Jorge Luis <jlrg1992@gmail.com>
2017-06-01 14:15:10 -0500

    Update random_quote_machine.js

--- random_quote_machine.js
+++ random_quote_machine.js
@@ -1,22 +1,29 @@
+var x = 0;
+var quoteData = null;
 function myQuote(){
   
+  if(x===0){
+$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(a) {
   
-$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  quoteData = a;
+}
   
-  var quoteData = a;
   
-  var yaQue = quoteData[0]["content"];
+  var yaQue = quoteData[x]["content"];
   yaQue = yaQue.split("");
    while(yaQue.length>75){
     yaQue.pop();
   }
   yaQue = yaQue.join("");
-  yaQue = yaQue+ "... -"+quoteData[0]["title"]+"- ";
+  yaQue = yaQue+ "... -"+quoteData[x]["title"]+"- ";
 
-   $("#citado").html(quoteData[0]["content"]);
-  $("#autor").html(quoteData[0]["title"]);
+   $("#citado").html(quoteData[x]["content"]);
+  $("#autor").html(quoteData[x]["title"]);
   $("#tuitera").attr("href",'https://twitter.com/intent/tweet?text='+yaQue+'See more at https://jorgerangel.ga');
 });
-
+  x++;
+  if(x===40){
+    x = 0;
+  }
   }
   myQuote();
