cd64cc44c752f9d87e4ba91eb7e9e1d27742a2a9
Jorge Luis <jlrg1992@gmail.com>
2017-06-01 12:23:42 -0500

    Add files via upload

---
+++ random_quote_machine.js
@@ -0,0 +1,22 @@
+function myQuote(){
+  
+  
+$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=https://jorgerangel.ga/random_quote_machine.html", function(a) {
+  
+  var quoteData = a;
+  
+  var yaQue = quoteData[0]["content"];
+  yaQue = yaQue.split("");
+   while(yaQue.length>75){
+    yaQue.pop();
+  }
+  yaQue = yaQue.join("");
+  yaQue = yaQue+ "... -"+quoteData[0]["title"]+"- ";
+
+   $("#citado").html(quoteData[0]["content"]);
+  $("#autor").html(quoteData[0]["title"]);
+  $("#tuitera").attr("href",'https://twitter.com/intent/tweet?text='+yaQue+'See more at https://jorgerangel.ga');
+});
+
+  }
+  myQuote();
