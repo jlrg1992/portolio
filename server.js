a0cd898aa9490594b3312da392ffb0a635a9d84c
jlrg1992 <jlrg1992@gmail.com>
2018-03-02 14:19:48 -0600

    static server added

---
+++ server.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES AND SETUP
+// ===============================================
+
+var express = require('express'),
+  app = express(),
+  path = require('path'),
+  port = Number(process.env.PORT || 8080);
+
+// ROUTES
+// ===============================================
+
+// Define the home page route.
+  app.use(express.static(path.join(__dirname, 'public')));
+// START THE SERVER
+// ===============================================
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
